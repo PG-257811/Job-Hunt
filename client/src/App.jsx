6fe2b905e94779e23c8f308534374e7a4dcac0cb
PG-257811 <praffulgupta09@gmail.com>
2023-06-07 16:04:58 +0530

    Sticky Footer ☄️

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
+import React from "react";
 import { Outlet, Navigate, Route, Routes, useLocation } from "react-router-dom";
-
 import { Footer, Navbar } from "./components";
 import {
   About,
@@ -20,44 +20,42 @@ function Layout() {
   return user?.token ? (
     <Outlet />
   ) : (
-    <Navigate to='/user-auth' state={{ from: location }} replace />
+    <Navigate to="/user-auth" state={{ from: location }} replace />
   );
 }
 
 function App() {
   const { user } = useSelector((state) => state.user);
   return (
-    <main className='bg-white'>
+    <div className="flex flex-col min-h-screen">
       <Navbar />
 
-      <Routes>
-        <Route element={<Layout />}>
-          <Route
-            path='/'
-            element={<Navigate to='/find-jobs' replace={true} />}
-          />
-          <Route path='/find-jobs' element={<FindJobs />} />
-          <Route path='/companies' element={<Companies />} />
-          <Route
-            path={
-              user?.accountType === "seeker"
-                ? "/user-profile"
-                : "/user-profile/:id"
-            }
-            element={<UserProfile />}
-          />
+      <main className="bg-white flex-grow">
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Navigate to="/find-jobs" replace={true} />} />
+            <Route path="/find-jobs" element={<FindJobs />} />
+            <Route path="/companies" element={<Companies />} />
+            <Route
+              path={
+                user?.accountType === "seeker"
+                  ? "/user-profile"
+                  : "/user-profile/:id"
+              }
+              element={<UserProfile />}
+            />
+            <Route path={"/company-profile"} element={<CompanyProfile />} />
+            <Route path={"/company-profile/:id"} element={<CompanyProfile />} />
+            <Route path={"/upload-job"} element={<UploadJob />} />
+            <Route path={"/job-detail/:id"} element={<JobDetail />} />
+          </Route>
+          <Route path="/about-us" element={<About />} />
+          <Route path="/user-auth" element={<AuthPage />} />
+        </Routes>
+      </main>
 
-          <Route path={"/company-profile"} element={<CompanyProfile />} />
-          <Route path={"/company-profile/:id"} element={<CompanyProfile />} />
-          <Route path={"/upload-job"} element={<UploadJob />} />
-          <Route path={"/job-detail/:id"} element={<JobDetail />} />
-        </Route>
-
-        <Route path='/about-us' element={<About />} />
-        <Route path='/user-auth' element={<AuthPage />} />
-      </Routes>
       {user && <Footer />}
-    </main>
+    </div>
   );
 }
 
