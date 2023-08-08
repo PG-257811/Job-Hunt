ff809f3ae7635313a39d74140804e40e99ecef14
PG-257811 <praffulgupta09@gmail.com>
2023-08-08 19:51:02 +0530

    Upload Job ✨

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -6,7 +6,20 @@ import SignUp from "./SignUp";
 import Header from "./Header";
 import ListBox from "./ListBox";
 import JobCard from "./JobCard";
-import CompanyCard from "./CompanyCard";
 import Loading from "./Loading";
+import CompanyCard from "./CompanyCard";
+import JobTypes from "./JobTypes";
 
-export { Navbar, Footer, CustomButton, TextInput, SignUp, Header, JobCard, ListBox, CompanyCard, Loading };
+export {
+  Navbar,
+  Footer,
+  CustomButton,
+  TextInput,
+  SignUp,
+  Header,
+  ListBox,
+  JobCard,
+  Loading,
+  CompanyCard,
+  JobTypes,
+};
