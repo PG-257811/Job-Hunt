89bcb302b324a0c40a4866e3fff65621aa6273ee
PG-257811 <praffulgupta09@gmail.com>
2023-08-11 18:08:07 +0530

    Error Middleware 🚀

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ import mongoSanitize from 'express-mongo-sanitize';
 
 import dbConnection from "./dbConfig/dbConnection.js";
 import router from './routes/index.js';
+import errorMiddleware from "./middlewares/errorMiddleware.js";
 
 dotenv.config()
 
@@ -31,6 +32,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
 app.use(router)
+
+//error middleware
+app.use(errorMiddleware);
 
 app.listen(PORT, () => {
   console.log(`Dev Server running on port: ${PORT}`);
